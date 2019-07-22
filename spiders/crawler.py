690e306f7c8ae36336a7027e7242c8be9b9a736a
tihomir.iliev <tisho.xmg@gmail.com>
2019-07-22 06:19:22 +0300

    second commit

--- spiders/crawler.py
+++ spiders/crawler.py
@@ -2,27 +2,24 @@
 import scrapy
 from scrapy_splash import SplashRequest
 from scrapy.shell import inspect_response
-import lxml.html as lh
-import pandas as pd
-import pudb
 
-class AppSpider(scrapy.Spider):
-    name = 'app'
+class DronesSpider(scrapy.Spider):
+    name = 'spider'
     
     allowed_domains = ['kamera-express.be']
     start_urls = ['https://www.kamera-express.be/producten/video/camera-drone/drone/#?page=1&size=48']
 
     def start_requests(self):
         for url in self.start_urls:
-            yield SplashRequest(url=url,endpoint="render.html",callback=self.parse,args={'wait':'3'})
+            yield SplashRequest(url = url, endpoint = "render.html", callback = self.parse, args = {'wait':'3'})
  
     def parse(self, response):
         second_page = 'https://www.kamera-express.be/producten/video/camera-drone/drone/#?page=2&size=48'
         links_list = [link.get() for link in response.xpath("//a/@ng-href")]
         for prod in links_list:
             product_link = 'https://www.kamera-express.be/' + prod 
-            yield SplashRequest(url=product_link, callback=self.extract_data, endpoint="render.html", args={"wait":"3"})
-        yield SplashRequest(url=second_page, callback=self.parse, endpoint="render.html", args={"wait":"3"})
+            yield SplashRequest(url = product_link, callback = self.extract_data, endpoint = "render.html", args = {"wait":"3"})
+        yield SplashRequest(url = second_page, callback = self.parse, endpoint = "render.html", args = {"wait":"3"})
        
         # inspect_response(response, self)
 
